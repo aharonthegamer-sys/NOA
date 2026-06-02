f2f83b3a46ed3e7f06104a60b34b95aeebc5b216
aharonthegamer-sys <aharonthegamer@gmail.com>
2026-06-02 23:57:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 from discord.ext import commands
+import discord
+intents = discord.Intents.default()
+intents.message_content = True
 from discord import Embed as AkagiEmbed
 from AkagiModules.Config.Config import reddit_client_id as RedditAkagiClientID
 from AkagiModules.Config.Config import reddit_client_secret as RedditAkagiClientSecret
