d09a122fea2658231944d2ca4fd1eeebdbdf7b62
aharonthegamer-sys <aharonthegamer@gmail.com>
2026-06-03 19:52:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -93,7 +93,8 @@ async def nsfw(ctx, category: str = None):
         await ctx.send(f"❌ בחר קטגוריה תקינה מהרשימה: {', '.join(valid_categories)}")
         return
 
-    url = f"https://purrbot.site/{category}/gif"
+    url = f"https://purrbot.site/api/img/nsfw
+/{category}/gif"
     headers = {"User-Agent": "Mozilla/5.0"}
     
     try:
